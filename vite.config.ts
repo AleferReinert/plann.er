5751f4cd09c93b124ff61a935677c6a1762c1e99
Alefer Reinert <aleferreinert@gmail.com>
2025-06-29 13:52:21 -0300

    perf: install and config unplugin-fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unfonts from 'unplugin-fonts/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		Unfonts({
+			google: {
+				families: ['Inter'],
+				display: 'swap'
+			}
+		})
+	]
 })
+

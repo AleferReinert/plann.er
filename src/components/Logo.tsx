97c0c5409f4a18c73054bc085c73d40600b71e39
Alefer Reinert <aleferreinert@gmail.com>
2025-06-29 14:09:00 -0300

    perf(Logo): add width and height

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
 	return (
 		<a href='/' className='inline-block mb-2'>
 			<h1>
-				<img src={logo} alt='plann.er' className='inline-block h-8' fetchPriority='high' />
+				<img src={logo} alt='plann.er' className='inline-block' width='157' height='33' fetchPriority='high' />
 			</h1>
 		</a>
 	)

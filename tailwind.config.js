97fdcb13332f4c6b787a8f4c46569c04e3576f37
Alefer Reinert <aleferreinert@gmail.com>
2025-06-29 15:06:18 -0300

    perf(background): re-add with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				squares: 'url(/img/background.webp)'
+			},
 			fontFamily: {
 				sans: 'Inter'
 			}

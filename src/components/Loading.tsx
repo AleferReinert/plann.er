3618a94490c1721483a435661734e930034715a8
Alefer Reinert <aleferreinert@gmail.com>
2025-06-29 11:49:58 -0300

    fix: replace h6 by p tag

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -11,7 +11,7 @@ export function Loading({ show, message }: LoadingProps) {
 				bg-zinc-950/95 fixed inset-0 z-50 transition flex flex-col gap-4 justify-center items-center
 		`}
 		>
-			<h6 className='text-lime-300 text-lg'>{message || 'Aguarde...'}</h6>
+			<p className='text-lime-300 text-lg'>{message || 'Aguarde...'}</p>
 			<div className='border-zinc-700 h-16 w-16 animate-spin rounded-full border-2 border-t-lime-300' />
 		</div>
 	)

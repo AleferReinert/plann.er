98aff4d61a6d93e2d67ce32ad4bb81a3f739609b
Alefer Reinert <aleferreinert@gmail.com>
2025-07-02 12:43:06 -0300

    feat: update to react 19 and fix ts error

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,8 @@ import logo from '/img/logo.svg'
 export function Logo() {
 	return (
 		<h1 className='inline-block mb-2'>
-			<img src={logo} alt='plann.er' className='inline-block' width='157' height='33' fetchPriority='high' />
+			{/* @ts-expect-error react error */}
+			<img src={logo} alt='plann.er' className='inline-block' width='157' height='33' fetchpriority='high' />
 		</h1>
 	)
 }

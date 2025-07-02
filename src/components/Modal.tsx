4c8a10fde6114dceeadb21bcd2355373f636c7a5
Alefer Reinert <aleferreinert@gmail.com>
2025-07-02 12:50:01 -0300

    feat(Modal): close on overlay click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,28 +11,28 @@ interface ModalProps {
 
 export function Modal({ title, description, children, closeModal, size = 'default' }: ModalProps) {
 	return (
-		<div className='fixed inset-0 flex justify-center items-center z-40 bg-black/60'>
+		<>
+			<div
+				className='fixed inset-0 flex justify-center items-center z-40 bg-black/60'
+				onClick={() => closeModal(false)}
+			/>
 			<section
 				className={`
 					${size === 'default' ? 'w-full max-w-[640px] ' : ''}
-					bg-zinc-900 px-6 py-5 rounded-xl
-				`}
+					bg-zinc-900 px-6 py-5 rounded-xl z-50 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+					`}
 			>
 				<header className='mb-5'>
 					<div className='flex justify-between items-start mb-2'>
 						<h2 className='text-white text-lg font-semibold leading-none'>{title}</h2>
 						<button title='Fechar'>
-							<X
-								className='text-zinc-400 hover:text-lime-300 transition'
-								size={20}
-								onClick={() => closeModal(false)}
-							/>
+							<X className='text-zinc-400 hover:text-lime-300 transition' size={20} onClick={() => closeModal(false)} />
 						</button>
 					</div>
 					<p className='text-zinc-400 text-sm'>{description}</p>
 				</header>
 				{children}
 			</section>
-		</div>
+		</>
 	)
 }
